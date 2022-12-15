bd5a6a0fff192f36e9d51e96e37474111dd03fd9
Ali Muradov <amuradov239@gmail.com>
2022-12-15 10:09:28 +0200

    initial commit
    first app commit plus shopping list

---
+++ AngularShoppingList/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShoppingListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
