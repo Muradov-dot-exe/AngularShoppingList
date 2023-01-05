1ef2d52ee225595194af9ee28657035b4fc5f1cb
Ali Muradov <amuradov239@gmail.com>
2023-01-05 10:32:04 +0200

    added add recipe buttons

--- AngularShoppingList/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ AngularShoppingList/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
--- AngularShoppingList/src/app/shopping-list/shopping-list.component.ts
+++ AngularShoppingList/src/app/shopping-list/shopping-list.component.ts
@@ -11,4 +11,8 @@ export class ShoppingListComponent {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
