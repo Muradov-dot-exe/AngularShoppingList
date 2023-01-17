6835b3802fa2079cc55e795e46f04c5241b38bf7
Ali Muradov <amuradov239@gmail.com>
2023-01-17 12:05:25 +0200

    added error handling

--- AngularShoppingList/src/app/auth/auth.component.ts
+++ AngularShoppingList/src/app/auth/auth.component.ts
@@ -31,9 +31,9 @@ export class AuthComponent {
           console.log(resData);
           this.isLoading = false;
         },
-        (error) => {
-          console.log(error);
-          this.error = 'An Error Ocurred';
+        (errorMessage) => {
+          console.log(errorMessage);
+          this.error = errorMessage;
           this.isLoading = false;
         }
       );
--- AngularShoppingList/src/app/auth/auth.service.ts
+++ AngularShoppingList/src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
   idToken: string;
@@ -16,10 +18,24 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(this.baseSignUpUrl, {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    });
+    return this.http
+      .post<AuthResponseData>(this.baseSignUpUrl, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error ocurred';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'Email already exists';
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 }
