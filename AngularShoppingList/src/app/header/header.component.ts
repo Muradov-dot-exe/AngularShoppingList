f35ee57025e0b6e8c8408702002baf29f10e03b6
Ali Muradov <amuradov239@gmail.com>
2023-01-17 15:14:17 +0200

    authentication updates

--- AngularShoppingList/src/app/header/header.component.ts
+++ AngularShoppingList/src/app/header/header.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  constructor(private dataStorage: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
+  constructor(
+    private dataStorage: DataStorageService,
+    private authService: AuthService
+  ) {}
 
   onSaveData() {
     this.dataStorage.storeRecipes();
   }
   onFetchData() {
     this.dataStorage.fetchRecipes().subscribe();
+  }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !user ? false : true;
+    });
+  }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 }
