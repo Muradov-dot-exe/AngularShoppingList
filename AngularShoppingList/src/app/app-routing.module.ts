db9279b6af62fd47da6d9097d6c11dca29ef7325
Ali Muradov <amuradov239@gmail.com>
2023-01-18 16:07:57 +0200

    Update app-routing.module.ts

--- AngularShoppingList/src/app/app-routing.module.ts
+++ AngularShoppingList/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -22,7 +22,9 @@ const appRoutes: Routes = [
   },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
